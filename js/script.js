e71b437df77c0104299f8f543b475480448d3662
ftf841 <ftf148@gmail.com>
2019-07-23 23:22:15 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++){
+    M.Dropdown.init(dropdowns[i]);
+}
+    $(document).ready(function() {
+        $('.parallax').parallax();
+    });
